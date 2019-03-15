3e6d0401864c57e9335743d1290e3d9348d97675
Michel Fortin <michel.fortin@michelf.com>
2019-03-15 07:22:59 -0400

    Moving review prompt in the *did*FinishLauching phase.

--- Mac App/AppDelegate.swift
+++ Mac App/AppDelegate.swift
@@ -23,11 +23,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 	func applicationWillFinishLaunching(_ notification: Notification) {
 		SimDaltonismFilter.registerDefaults()
-		ReviewPrompt.promptForReview(afterDelay: 2.3, minimumNumberOfLaunches: 10, minimumDaysSinceFirstLaunch: 6, minimumDaysSinceLastPrompt: 365)
 	}
 
 	func applicationDidFinishLaunching(_ notification: Notification) {
 		FilterWindowManager.shared.showFirstWindow()
+		ReviewPrompt.promptForReview(afterDelay: 2.3, minimumNumberOfLaunches: 10, minimumDaysSinceFirstLaunch: 6, minimumDaysSinceLastPrompt: 365)
 	}
 
 	func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
