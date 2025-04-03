2fd1a26bd2ef221418975feb79baebec5822b3df
Michel Fortin <michel.fortin@michelf.com>
2025-04-03 08:55:54 -0400

    Removing review prompt.

--- Mac App/AppDelegate.swift
+++ Mac App/AppDelegate.swift
@@ -27,7 +27,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 	func applicationDidFinishLaunching(_ notification: Notification) {
 		FilterWindowManager.shared.showFirstWindow()
-		ReviewPrompt.promptForReview(afterDelay: 2.3, minimumNumberOfLaunches: 10, minimumDaysSinceFirstLaunch: 6, minimumDaysSinceLastPrompt: 365)
 	}
 
 	func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
