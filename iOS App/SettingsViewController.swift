090856a23ff451544d01791a8cdb89a0288330cb
Michel Fortin <michel.fortin@michelf.com>
2025-04-13 23:16:07 -0400

    iOS: adding controls for toggling vision tools.

--- iOS App/SettingsViewController.swift
+++ iOS App/SettingsViewController.swift
@@ -7,6 +7,13 @@ class SettingsViewController: UIViewController {
 	@IBOutlet var visionTypeName: UILabel?
 	@IBOutlet var visionTypeDescription: UILabel?
 
+	@IBOutlet var redStripeSwitch:  UISwitch!
+	@IBOutlet var greenStripeSwitch:  UISwitch!
+	@IBOutlet var blueStripeSwitch:  UISwitch!
+	@IBOutlet var invertHueSwitch:  UISwitch!
+	@IBOutlet var invertLuminanceSwitch:  UISwitch!
+	@IBOutlet var saturationBoostSwitch:  UISwitch!
+
 	deinit {
 		NotificationCenter.default.removeObserver(self)
 	}
@@ -41,10 +48,43 @@ class SettingsViewController: UIViewController {
 		// Sim Daltonism
 		self.visionTypeName!.text = FilterStore.global.configuration.vision.name
 		self.visionTypeDescription!.text = FilterStore.global.configuration.vision.description
+
+		// Vision Tools
+		redStripeSwitch.isOn = FilterStore.global.configuration.stripeConfig.redStripes != 0
+		greenStripeSwitch.isOn = FilterStore.global.configuration.stripeConfig.greenStripes != 0
+		blueStripeSwitch.isOn = FilterStore.global.configuration.stripeConfig.blueStripes != 0
+		invertHueSwitch.isOn = FilterStore.global.configuration.hueShift
+		invertLuminanceSwitch.isOn = FilterStore.global.configuration.invertLuminance
+		saturationBoostSwitch.isOn = FilterStore.global.configuration.colorBoost
 	}
 
 	@IBAction func closeSettings(_ sender: Any) {
 		self.presentingViewController?.dismiss(animated: true)
+	}
+
+
+	@IBAction func redStripeSwitchToggled(_ sender: Any) {
+		FilterStore.global.configuration.stripeConfig.redStripes = redStripeSwitch.isOn ? 2 : 0
+	}
+
+	@IBAction func greenStripeSwitchToggled(_ sender: Any) {
+		FilterStore.global.configuration.stripeConfig.greenStripes = greenStripeSwitch.isOn ? 2 : 0
+	}
+
+	@IBAction func blueStripeSwitchToggled(_ sender: Any) {
+		FilterStore.global.configuration.stripeConfig.blueStripes = blueStripeSwitch.isOn ? 2 : 0
+	}
+
+	@IBAction func invertHueSwitchToggled(_ sender: Any) {
+		FilterStore.global.configuration.hueShift = invertHueSwitch.isOn
+	}
+
+	@IBAction func invertLuminanceSwitchToggled(_ sender: Any) {
+		FilterStore.global.configuration.invertLuminance = invertLuminanceSwitch.isOn
+	}
+
+	@IBAction func saturationBoostSwitchToggled(_ sender: Any) {
+		FilterStore.global.configuration.colorBoost = saturationBoostSwitch.isOn
 	}
 
 }
