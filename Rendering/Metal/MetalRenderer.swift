4a5fa58ad1215be18ebb7a5c08008e2669a4ffc7
Michel Fortin <michel.fortin@michelf.com>
2025-06-06 21:41:03 -0400

    Implementing scaling as an aspect-fit transform, preserving aspect ratio. Most needed on iOS with the camera.

--- Rendering/Metal/MetalRenderer.swift
+++ Rendering/Metal/MetalRenderer.swift
@@ -97,12 +97,23 @@ extension MetalRenderer: MTKViewDelegate {
     private func rescaleIfNeeded(drawableSize: CGSize, imageExtent: CGRect) {
         guard drawableSize != imageExtent.size else { return }
 
-        let scaling = CGAffineTransform(
-            scaleX: drawableSize.width / imageExtent.width,
-            y: drawableSize.height / imageExtent.height
-        )
+		// aspect-fit transform
+		let ratioX = drawableSize.width / imageExtent.width
+		let ratioY = drawableSize.height / imageExtent.height
+		let scale: CGFloat
+		var translationX: CGFloat = 0
+		var translationY: CGFloat = 0
+		if ratioX < ratioY {
+			scale = ratioY
+			translationX -= round((imageExtent.width * scale - drawableSize.width) / 2)
+		} else {
+			scale = ratioX
+			translationY -= round((imageExtent.height * scale - drawableSize.height) / 2)
+		}
 
-        image = image.transformed(by: scaling, highQualityDownsample: false)
+		let transform = CGAffineTransform(scaleX: scale, y: scale).translatedBy(x: translationX, y: translationY)
+
+        image = image.transformed(by: transform, highQualityDownsample: false)
 
     }
     
--- Rendering/OpenGL/OpenGLRenderer.swift
+++ Rendering/OpenGL/OpenGLRenderer.swift
@@ -77,11 +77,36 @@ extension OpenGLRenderer {
 		let drawableSize = self.drawableSize
 		drawableSizeLock.unlock()
 
+		rescaleIfNeeded(drawableSize: drawableSize, imageExtent: image.extent)
+
 		let drawableRect = CGRect(origin: .zero, size: drawableSize)
 
 		view.openGLContext!.makeCurrentContext()
 		context?.draw(image, in: drawableRect, from: image.extent)
 		glFlush()
     }
+
+	private func rescaleIfNeeded(drawableSize: CGSize, imageExtent: CGRect) {
+		guard drawableSize != imageExtent.size else { return }
+
+		// aspect-fit transform
+		let ratioX = drawableSize.width / imageExtent.width
+		let ratioY = drawableSize.height / imageExtent.height
+		let scale: CGFloat
+		var translationX: CGFloat = 0
+		var translationY: CGFloat = 0
+		if ratioX < ratioY {
+			scale = ratioY
+			translationX -= round((imageExtent.width * scale - drawableSize.width) / 2)
+		} else {
+			scale = ratioX
+			translationY -= round((imageExtent.height * scale - drawableSize.height) / 2)
+		}
+
+		let transform = CGAffineTransform(scaleX: scale, y: scale).translatedBy(x: translationX, y: translationY)
+
+		image = image.transformed(by: transform, highQualityDownsample: false)
+
+	}
 
 }
