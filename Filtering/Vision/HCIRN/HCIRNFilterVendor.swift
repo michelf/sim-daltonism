abe25acd665bfbeab9026e567e221a1d4dd0507a
Michel Fortin <michel.fortin@michelf.com>
2025-07-21 16:18:53 -0400

    Updated license header.

--- Filtering/Vision/HCIRN/HCIRNFilterVendor.swift
+++ Filtering/Vision/HCIRN/HCIRNFilterVendor.swift
@@ -1,5 +1,5 @@
 
-//    Copyright 2005-2017 Michel Fortin
+//    Copyright 2005-2025 Michel Fortin
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
